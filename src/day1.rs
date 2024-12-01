5854ffaafa1e10178945f5fdc8e7ac9456e5559d
Paul Mealor <paul.mealor@gmail.com>
2024-12-01 18:58:41 +0000

    Day 1 part 2

--- src/day1.rs
+++ src/day1.rs
@@ -27,11 +27,33 @@ impl days::Day for Day {
 
         Some(result)
     }
+    fn part2(&self, input: &str) -> Option<i64> {
+        let mut left = vec![];
+        let mut right = vec![];
+
+        for line in input.split("\n") {
+            let mut parts = line.split_whitespace();
+            left.push(parts.next().unwrap().parse::<i64>().unwrap());
+            right.push(parts.next().unwrap().parse::<i64>().unwrap());
+        }
+
+        left.sort();
+        right.sort();
+
+        let mut result = 0;
+
+        for l in left {
+            let i = right.partition_point(|&r| r < l);
+            let j = right.partition_point(|&r| r <= l);
+            result += l * (j - i) as i64;
+        }
+
+        Some(result)
+    }
 }
 
 #[cfg(test)]
 mod tests {
-
     use crate::days::Day;
 
     const DAY: crate::day1::Day = crate::day1::Day;
@@ -44,5 +66,15 @@ mod tests {
 3   9
 3   3";
         assert_eq!(DAY.part1(text), Some(11))
+    }
+    #[test]
+    fn part2_example1() {
+        let text = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+        assert_eq!(DAY.part2(text), Some(31))
     }
 }
