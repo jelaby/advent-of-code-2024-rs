4b0a716e49febaf17609eacef0a4edfedf00c86c
Paul Mealor <paul.mealor@gmail.com>
2024-12-03 09:25:06 +0000

    Day 3 part 1

--- src/day3.rs
+++ src/day3.rs
@@ -1,4 +1,5 @@
 use crate::days;
+use regex::Regex;
 
 pub struct Day;
 
@@ -10,7 +11,12 @@ impl days::Day for Day {
     }
 
     fn part1(&self, input: &str) -> Option<i64> {
-        None
+
+        let mul_pattern = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+        Some(mul_pattern.captures_iter(input).map(|c| c.extract())
+            .map(|(_, [left, right])| left.parse::<i64>().unwrap() * right.parse::<i64>().unwrap())
+            .sum())
     }
     fn part2(&self, input: &str) -> Option<i64> {
         None
@@ -21,11 +27,11 @@ impl days::Day for Day {
 mod tests {
     use crate::days::Day;
 
-    const DAY: crate::day2::Day = crate::day2::Day;
+    const DAY: crate::day3::Day = crate::day3::Day;
     #[test]
     fn part1_example1() {
-        let text = "";
-        assert_eq!(DAY.part1(text), Some(2))
+        let text = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        assert_eq!(DAY.part1(text), Some(161))
     }
     #[test]
     fn part2_example1() {
