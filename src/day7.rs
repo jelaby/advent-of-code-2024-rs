05a5d56ea76cb072fb2aa0009aeb96da2337301d
Paul Mealor <paul.mealor@gmail.com>
2024-12-07 22:30:41 +0000

    Day 7 part 2

--- src/day7.rs
+++ src/day7.rs
@@ -21,8 +21,8 @@ fn parse(input: &str) -> Vec<(i64, Vec<i64>)> {
         .collect()
 }
 
-fn is_possible(total: i64, items: &Vec<i64>) -> bool {
-    is_possible_n(total, &items[1..items.len()], items[0])
+fn is_possible(total: i64, items: &Vec<i64>, ops: &Vec<fn(i64, i64) -> i64>) -> bool {
+    is_possible_n(total, &items[1..items.len()], ops, items[0])
 }
 
 fn add(a: i64, b: i64) -> i64 {
@@ -31,15 +31,25 @@ fn add(a: i64, b: i64) -> i64 {
 fn mul(a: i64, b: i64) -> i64 {
     a * b
 }
+fn cat(a: i64, b: i64) -> i64 {
+    let mut a = a;
+    let mut n = b;
+    while n > 0 {
+        n = n / 10;
+        a = a * 10;
+    }
+    a + b
+}
 
-const OP: [fn(i64, i64) -> i64; 2] = [add, mul];
+const OP: [fn(i64,i64)->i64; 2] = [add, mul];
+const OP2: [fn(i64,i64)->i64; 3] = [add, mul, cat];
 
-fn is_possible_n(target: i64, items: &[i64], total: i64) -> bool {
+fn is_possible_n(target: i64, items: &[i64], ops: &Vec<fn(i64,i64) -> i64>, total: i64) -> bool {
     if items.is_empty() {
         return target == total;
     }
-    for op in OP {
-        if is_possible_n(target, &items[1..items.len()], op(total, items[0])) {
+    for op in ops {
+        if is_possible_n(target, &items[1..items.len()], ops, op(total, items[0])) {
             return true;
         }
     }
@@ -57,13 +67,21 @@ impl days::Day for Day {
         Some(
             equations
                 .iter()
-                .filter(|(total, values)| is_possible(*total, values))
+                .filter(|(total, values)| is_possible(*total, values, &OP.to_vec()))
                 .map(|(total, _)| total)
-                .sum(),
+                .sum()
         )
     }
     fn part2(&self, input: &str) -> Option<i64> {
-        None
+        let equations = parse(input);
+
+        Some(
+            equations
+                .iter()
+                .filter(|(total, values)| is_possible(*total, values, &OP2.to_vec()))
+                .map(|(total, _)| total)
+                .sum()
+        )
     }
 }
 
@@ -89,7 +107,17 @@ mod tests {
     }
     #[test]
     fn part2_example1() {
-        let text = "";
-        assert_eq!(DAY.part2(text), Some(4))
+        let text = "\
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20\
+";
+        assert_eq!(DAY.part2(text), Some(11387))
     }
 }
