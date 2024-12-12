750c4a86c8328944213349406b4882b9771e69f0
Paul Mealor <paul.mealor@gmail.com>
2024-12-12 13:32:12 +0000

    Day 12 part 2

--- src/day12.rs
+++ src/day12.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+use itertools::Itertools;
 use crate::days;
 
 pub struct Day;
@@ -33,7 +35,7 @@ fn count(map: &Vec<Vec<char>>, x: usize, y: usize, visited: &mut Vec<Vec<bool>>)
             boundary += 1;
         } else {
             let (neighbour_area, neighbour_boundary) =
-                count(map, neighbour_x as usize, neighbour_y as usize, visited);
+                crate::day12::count(map, neighbour_x as usize, neighbour_y as usize, visited);
             area += neighbour_area;
             boundary += neighbour_boundary;
         }
@@ -42,14 +44,116 @@ fn count(map: &Vec<Vec<char>>, x: usize, y: usize, visited: &mut Vec<Vec<bool>>)
     (area, boundary)
 }
 
+
 fn count_all(map: &Vec<Vec<char>>) -> usize{
     let mut visited = vec![vec![false; map[0].len()]; map.len()];
     let mut score = 0;
 
     for y in 0..map.len() {
         for x in 0..map[y].len() {
-            let (next_area, next_boundary) = count(&map, x, y, &mut visited);
+            let (next_area, next_boundary) = crate::day12::count(&map, x, y, &mut visited);
             score += next_area * next_boundary;
+        }
+    }
+
+    score
+}
+
+fn calculate_area(map: &Vec<Vec<char>>, x: usize, y: usize, visited: &mut Vec<Vec<bool>>) -> usize {
+    if visited[y][x] {
+        return 0;
+    }
+    visited[y][x] = true;
+    let mut area = 1;
+    let t = map[y][x];
+
+    for (dx, dy) in DIR {
+        let neighbour_x = x as i64 + dx;
+        let neighbour_y = y as i64 + dy;
+
+        if neighbour_x < 0
+            || neighbour_x >= map[y].len() as i64
+            || neighbour_y < 0
+            || neighbour_y >= map.len() as i64
+            || map[neighbour_y as usize][neighbour_x as usize] != t
+        {} else {
+            area += calculate_area(map, neighbour_x as usize, neighbour_y as usize, visited);
+        }
+    }
+    area
+}
+
+fn is_same(map: &Vec<Vec<char>>, t: char, x:i64, y:i64) -> bool {
+    x >= 0 && x < map[0].len() as i64 && y >= 0 && y < map.len() as i64 && map[y as usize][x as usize] == t
+}
+
+fn count_sides(map: &Vec<Vec<char>>, x: usize, y: usize, visited: &mut Vec<Vec<HashSet<(i64,i64)>>>) -> usize {
+
+    let mut result = 0;
+    let t = map[y][x];
+
+    for dir in &DIR {
+        if !visited[y][x].iter().contains(&dir) {
+            visited[y][x].insert(dir.clone());
+
+            let (dx,dy) = *dir;
+
+            let neighbour_x = x as i64 + dx;
+            let neighbour_y = y as i64 + dy;
+
+            if !is_same(map, t, neighbour_x, neighbour_y) {
+                result += 1;
+                trace_edge(&map, x, y, dy,dx, dx, dy, visited);
+                trace_edge(&map, x, y, -dy,-dx, dx, dy, visited);
+            } else {
+                result += count_sides(map, neighbour_x as usize, neighbour_y as usize, visited);
+            }
+        }
+    }
+
+    result
+}
+
+fn trace_edge(map: &Vec<Vec<char>>, x: usize, y: usize, dx: i64, dy: i64, wallx:i64, wally:i64, visited: &mut Vec<Vec<HashSet<(i64,i64)>>>) {
+
+    visited[y][x].insert((wallx,wally));
+
+    let next_x = x as i64 + dx;
+    let next_y = y as i64 + dy;
+
+    let t = map[y][x];
+
+    if !is_same(&map, t, next_x, next_y) {
+        // reached an inside corner -- stop tracing
+        return;
+
+    }
+
+    // is the wall still there?
+    let over_wall_x = next_x + wallx;
+    let over_wall_y = next_y + wally;
+
+    if is_same(map, t, over_wall_x, over_wall_y) {
+        // end of the wall -- stop tracing
+        return
+    }
+
+    // still on a wall, so trace along it further
+    trace_edge(&map, next_x as usize, next_y as usize, dx, dy, wallx, wally, visited);
+}
+
+fn count_all_2(map: &Vec<Vec<char>>) -> usize{
+    let mut visited = vec![vec![false; map[0].len()]; map.len()];
+    let mut visited2 = vec![vec![HashSet::new(); map[0].len()]; map.len()];
+    let mut score = 0;
+
+    for y in 0..map.len() {
+        for x in 0..map[y].len() {
+            let next_area = calculate_area(map,x,y,&mut visited);
+            if next_area > 0 {
+                let sides = count_sides(&map, x, y, &mut visited2);
+                score += next_area * sides;
+            }
         }
     }
 
@@ -67,7 +171,8 @@ impl days::Day for Day {
         Some(count_all(&map) as i64)
     }
     fn part2(&self, input: &str) -> Option<i64> {
-        None
+        let map = parse(input);
+        Some(count_all_2(&map) as i64)
     }
 }
 
@@ -112,7 +217,62 @@ OOOOO";
     }
     #[test]
     fn part2_example1() {
-        let text = "";
+        let text = "\
+AAAA
+BBCD
+BBCC
+EEEC";
+        assert_eq!(DAY.part2(text), Some(80))
+    }
+    #[test]
+    fn part2_example2() {
+        let text = "\
+EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE";
+        assert_eq!(DAY.part2(text), Some(236))
+    }
+    #[test]
+    fn part2_example3() {
+        let text = "\
+AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA";
+        assert_eq!(DAY.part2(text), Some(368))
+    }
+    #[test]
+    fn part2_example4() {
+        let text = "\
+RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE";
+        assert_eq!(DAY.part2(text), Some(1206))
+    }
+    #[test]
+    fn part2_small() {
+        let text = "O";
         assert_eq!(DAY.part2(text), Some(4))
+    }
+    #[test]
+    fn part2_small2() {
+        let text = "OO";
+        assert_eq!(DAY.part2(text), Some(8))
+    }
+    #[test]
+    fn part2_two() {
+        let text = "AB";
+        assert_eq!(DAY.part2(text), Some(8))
     }
 }
