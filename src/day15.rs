b07e74ebfe5cc8ef6ee2da6c5734b7e27ac580e3
Paul Mealor <paul.mealor@gmail.com>
2024-12-17 09:22:37 +0000

    Day 15 part 2

--- src/day15.rs
+++ src/day15.rs
@@ -1,7 +1,7 @@
 use crate::day15::Block::*;
 use crate::day15::Dir::*;
 use crate::days;
-use nalgebra::{Scalar, Vector2};
+use nalgebra::{Vector2};
 use num::{Num, NumCast};
 
 pub struct Day;
@@ -14,6 +14,8 @@ enum Block {
     ROBOT,
     WALL,
     CRATE,
+    CRATE_L,
+    CRATE_R,
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -24,7 +26,7 @@ enum Dir {
     DOWN,
 }
 
-fn parse(input: &str) -> (Vec<Vec<Block>>, Vector2<usize>, Vec<Dir>) {
+fn parse(input: &str, part2: bool) -> (Vec<Vec<Block>>, Vector2<usize>, Vec<Dir>) {
     let mut parts = input.split_terminator("\n\n");
 
     let mut map: Vec<Vec<Block>> = parts
@@ -43,6 +45,8 @@ fn parse(input: &str) -> (Vec<Vec<Block>>, Vector2<usize>, Vec<Dir>) {
                 .collect()
         })
         .collect();
+
+    let mut map = if part2 { to_part2(&map) } else { map };
 
     let robot = map
         .iter()
@@ -74,6 +78,22 @@ fn parse(input: &str) -> (Vec<Vec<Block>>, Vector2<usize>, Vec<Dir>) {
     (map, robot, commands)
 }
 
+fn to_part2(map: &Vec<Vec<Block>>) -> Vec<Vec<Block>> {
+    map.iter()
+        .map(|row| {
+            row.iter()
+                .flat_map(|block| match block {
+                    SPACE => [SPACE, SPACE].into_iter(),
+                    ROBOT => [ROBOT, SPACE].into_iter(),
+                    WALL => [WALL, WALL].into_iter(),
+                    CRATE => [CRATE_L, CRATE_R].into_iter(),
+                    _ => panic!("Unexpected {block:?} while converting to part 2"),
+                })
+                .collect()
+        })
+        .collect()
+}
+
 fn vec_for(dir: Dir) -> Vector2<i64> {
     match dir {
         LEFT => Vector2::new(-1, 0),
@@ -83,27 +103,78 @@ fn vec_for(dir: Dir) -> Vector2<i64> {
     }
 }
 
-fn move_block(map: &mut Vec<Vec<Block>>, p: Vector2<usize>, v: Vector2<i64>) -> bool {
+fn can_move_block(map: &mut Vec<Vec<Block>>, p: Vector2<usize>, v: Vector2<i64>) -> bool {
     let n = (p.cast() + v).map(|i| i as usize);
+    let r = Vector2::new(1, 0);
 
-    match map[n.y][n.x] {
-        SPACE => {
-            map[n.y][n.x] = map[p.y][p.x];
-            map[p.y][p.x] = SPACE;
-            true
-        }
+    match map[p.y][p.x] {
+        SPACE => true,
         WALL => false,
-        CRATE => {
-            if move_block(map, n, v) {
-                map[n.y][n.x] = map[p.y][p.x];
-                map[p.y][p.x] = SPACE;
-                true
+        CRATE => can_move_block(map, n, v),
+        CRATE_L => {
+            if v.y == 0 {
+                if v.x == 1 {
+                    can_move_block(map, n + r, v)
+                } else {
+                    can_move_block(map, n, v)
+                }
             } else {
-                false
+                can_move_block(map, n, v) && can_move_block(map, n + r, v)
             }
         }
-        ROBOT => panic!("Unexpected robot at {p:?}+{v:?} => {n:?}"),
+        CRATE_R => can_move_block(map, p - r, v),
+        ROBOT => can_move_block(map, n, v),
     }
+}
+
+fn move_block(map: &mut Vec<Vec<Block>>, p: Vector2<usize>, v: Vector2<i64>) -> bool {
+    let n = (p.cast() + v).map(|i| i as usize);
+    let r = Vector2::new(1, 0);
+
+    if !can_move_block(map, p, v) {
+        return false;
+    }
+
+    match map[p.y][p.x] {
+        SPACE => {
+        }
+        WALL => panic!("Tried to move wall at {p:?}+{v:?} => {n:?}"),
+        CRATE => {
+            move_block(map, n, v);
+            map[n.y][n.x] = map[p.y][p.x];
+            map[p.y][p.x] = SPACE;
+        }
+        CRATE_L => {
+            if v.x == 1 {
+                move_block(map, n + r, v);
+                map[n.y][n.x + 1] = map[n.y][n.x];
+                map[n.y][n.x] = map[p.y][p.x];
+                map[p.y][p.x] = SPACE;
+            } else if v.x == -1 {
+                move_block(map, n, v);
+                map[n.y][n.x] = map[p.y][p.x];
+                map[p.y][p.x] = map[p.y][p.x + 1];
+                map[p.y][p.x + 1] = SPACE;
+            } else {
+                move_block(map, n + r, v);
+                move_block(map, n, v);
+                map[n.y][n.x] = map[p.y][p.x];
+                map[n.y][n.x + 1] = map[p.y][p.x + 1];
+                map[p.y][p.x] = SPACE;
+                map[p.y][p.x + 1] = SPACE;
+            }
+        }
+        CRATE_R => {
+            move_block(map, p - r, v);
+        }
+        ROBOT => {
+            move_block(map, n, v);
+            map[n.y][n.x] = map[p.y][p.x];
+            map[p.y][p.x] = SPACE;
+        }
+    }
+
+    true
 }
 
 fn score(map: &Vec<Vec<Block>>) -> usize {
@@ -112,22 +183,28 @@ fn score(map: &Vec<Vec<Block>>) -> usize {
         .flat_map(|(y, row)| {
             row.iter()
                 .enumerate()
-                .map(move |(x, c)| if matches!(c, CRATE) { y * 100 + x } else { 0 })
+                .map(move |(x, c)| if matches!(c, CRATE) || matches!(c, CRATE_L) { y * 100 + x } else { 0 })
         })
         .sum()
 }
 
-fn show_map(map: &Vec<Vec<Block>>) {
+fn show_map(map: &Vec<Vec<Block>>, robot: Vector2<usize>) {
     println!();
-    for row in map {
-        for block in row {
+    for (y,row) in map.iter().enumerate() {
+        for (x,block) in row.iter().enumerate() {
             print!(
                 "{}",
-                match block {
-                    SPACE => ' ',
-                    ROBOT => '@',
-                    CRATE => 'O',
-                    WALL => '#',
+                if x == robot.x && y == robot.y {
+                    '@'
+                } else {
+                    match block {
+                        SPACE => ' ',
+                        ROBOT => '*',
+                        CRATE => 'O',
+                        WALL => '#',
+                        CRATE_L => '[',
+                        CRATE_R => ']',
+                    }
                 }
             );
         }
@@ -141,23 +218,30 @@ impl days::Day for Day {
     }
 
     fn part1(&self, input: &str) -> Option<i64> {
-        let (mut map, mut pos, commands) = parse(input);
+        let (mut map, mut pos, commands) = parse(input, false);
 
-        show_map(&map);
         for command in commands {
             let dir = vec_for(command);
 
             if move_block(&mut map, pos, dir) {
                 pos = (pos.cast() + dir).map(|i| i as usize);
             }
-
-            //show_map(&map);
         }
 
         Some(score(&map) as i64)
     }
     fn part2(&self, input: &str) -> Option<i64> {
-        None
+        let (mut map, mut pos, commands) = parse(input, true);
+
+        for command in commands {
+            let dir = vec_for(command);
+
+            if move_block(&mut map, pos, dir) {
+                pos = (pos.cast() + dir).map(|i| i as usize);
+            }
+        }
+
+        Some(score(&map) as i64)
     }
 }
 
@@ -209,7 +293,42 @@ v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^";
     }
     #[test]
     fn part2_example1() {
-        let text = "";
-        assert_eq!(DAY.part2(text), Some(4))
+        let text = "\
+#######
+#...#.#
+#.....#
+#..OO@#
+#..O..#
+#.....#
+#######
+
+<vv<<^^<<^^";
+        assert_eq!(DAY.part2(text), Some(105 + 207 + 306))
+    }
+    #[test]
+    fn part2_example2() {
+        let text = "\
+##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^";
+        assert_eq!(DAY.part2(text), Some(9021))
     }
 }
