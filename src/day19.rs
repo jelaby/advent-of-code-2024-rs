e8d185dfc048a729d7716738f822c1476c45935f
Paul Mealor <paul.mealor@gmail.com>
2024-12-19 11:09:24 +0000

    Day 19 part 2

--- src/day19.rs
+++ src/day19.rs
@@ -1,22 +1,52 @@
+use crate::days;
 use itertools::Itertools;
 use regex::Regex;
-use crate::days;
+use std::cmp::min;
+use std::collections::{HashMap, HashSet};
 
 pub struct Day;
 
 impl Day {}
 
-fn parse<'a>(input: &'a str) -> (Regex, Vec<&'a str>) {
+fn parse<'a>(input: &'a str) -> (Vec<&'a str>, Vec<&'a str>) {
     let mut lines = input.lines();
 
-    let towels = lines.next().unwrap();
-    let towels = Regex::new(&format!("^({})+$", towels.split(", ").join("|"))).unwrap();
+    let towels: Vec<&str> = lines.next().unwrap().split(", ").collect();
 
     let _ = lines.next();
 
-    let designs = lines.collect();
+    let designs = lines.sorted().collect();
 
     (towels, designs)
+}
+
+fn count_combinations<'a>(
+    cache: &mut HashMap<&'a str, usize>,
+    towels: &HashSet<&str>,
+    min_towel: usize,
+    max_towel: usize,
+    design: &'a str,
+) -> usize {
+    if design.is_empty() {
+        1
+    } else {
+        match cache.get(design) {
+            Some(result) => *result,
+            None => {
+                let mut result = 0;
+
+                for len in min_towel..=min(max_towel, design.len()) {
+                    if towels.contains(&design[0..len]) {
+                        result +=
+                            count_combinations(cache, towels, min_towel, max_towel, &design[len..])
+                    }
+                }
+                cache.insert(design, result);
+
+                result
+            }
+        }
+    }
 }
 
 impl days::Day for Day {
@@ -26,18 +56,38 @@ impl days::Day for Day {
 
     fn part1(&self, input: &str) -> Option<String> {
         let (towels, designs) = parse(input);
+        let towels = Regex::new(&format!("^({})+$", towels.iter().join("|"))).unwrap();
 
-        Some(designs.iter()
-            .filter(|design| towels.is_match(design))
-            .count()).map(|r| r.to_string())
+        Some(
+            designs
+                .iter()
+                .filter(|design| towels.is_match(design))
+                .count(),
+        )
+        .map(|r| r.to_string())
     }
     fn part2(&self, input: &str) -> Option<String> {
-        None
+        let (towels, designs) = parse(input);
+        let towels = HashSet::from_iter(towels);
+
+        let min_towel = towels.iter().map(|towel| towel.len()).min().unwrap();
+        let max_towel = towels.iter().map(|towel| towel.len()).max().unwrap();
+
+        Some(
+            designs
+                .iter()
+                .map(|design| {
+                    count_combinations(&mut HashMap::new(), &towels, min_towel, max_towel, design)
+                })
+                .sum::<usize>(),
+        )
+        .map(|r| r.to_string())
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use crate::days::Day;
 
     const DAY: super::Day = super::Day;
@@ -58,7 +108,24 @@ bbrgwb";
     }
     #[test]
     fn part2_example1() {
-        let text = "";
-        assert_eq!(DAY.part2(text), Some("4".to_string()))
+        let text = "\
+r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb";
+        assert_eq!(DAY.part2(text), Some(16.to_string()))
+    }
+    #[test]
+    fn count_combinations_short() {
+        assert_eq!(
+            count_combinations(&mut HashMap::new(), &HashSet::from(["a"]), 1, 1, "a"),
+            1
+        );
     }
 }
